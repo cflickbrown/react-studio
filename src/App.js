03769642ad30aa40d858e2dc74228acf09554a4f
cflickbrown <connor_flick@brown.edu>
2024-03-15 16:54:56 -0400

    primary commit

--- src/App.js
+++ src/App.js
@@ -11,19 +11,45 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([])
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <div class="main-content">
+        <div class="menu">
+        {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+          <div class="BakeryItem">
+            <img src={item.image}></img>
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+            <div class="purchase-zone">
+              <h4>{item.price}</h4>
+              <button onClick={e => setCart([...cart, item])}>Add to Cart</button>
+            </div>
+          </div> // replace with BakeryItem component
+        ))}
+        </div>
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+        <div class="cart">
+          <h2>Cart</h2>
+          {
+            <div>
+              Total: <b>${cart.reduce((prev, curr) => parseFloat((prev + parseFloat(curr.price)).toFixed(2)), 0.00)}</b>
+            </div>
+          }
+          {/* TODO: render a list of items in the cart */
+            cart.map(x => 
+            <p>
+              {x.name}: {x.price}
+            </p>
+            ).reverse()
+          }
+          
+        </div>
 
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
       </div>
+      
     </div>
   );
 }
